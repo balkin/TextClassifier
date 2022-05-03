81f0e30e0013a085ff0a361e5aecff34cb47ece3
Ruslan Balkin <baron@baron.su>
2022-05-03 23:25:11 +0300

    Minor refactoring

--- src/main/java/com/irvil/textclassifier/VocabularyBuilder.java
+++ src/main/java/com/irvil/textclassifier/VocabularyBuilder.java
@@ -9,59 +9,55 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 class VocabularyBuilder {
-  private final NGramStrategy nGramStrategy;
 
-  VocabularyBuilder(NGramStrategy nGramStrategy) {
-    if (nGramStrategy == null) {
-      throw new IllegalArgumentException();
-    }
+    private final NGramStrategy nGramStrategy;
 
-    this.nGramStrategy = nGramStrategy;
-  }
-
-  List<VocabularyWord> getVocabulary(List<ClassifiableText> classifiableTexts) {
-    if (classifiableTexts == null ||
-        classifiableTexts.size() == 0) {
-      throw new IllegalArgumentException();
-    }
-
-    Map<String, Integer> uniqueValues = new HashMap<>();
-
-    // count frequency of use each word (converted to n-gram) from all Classifiable Texts
-    //
-
-    for (ClassifiableText classifiableText : classifiableTexts) {
-      for (String word : nGramStrategy.getNGram(classifiableText.getText())) {
-        if (uniqueValues.containsKey(word)) {
-          // increase counter
-          uniqueValues.put(word, uniqueValues.get(word) + 1);
-        } else {
-          // add new word
-          uniqueValues.put(word, 1);
+    VocabularyBuilder(NGramStrategy nGramStrategy) {
+        if (nGramStrategy == null) {
+            throw new IllegalArgumentException();
         }
-      }
+
+        this.nGramStrategy = nGramStrategy;
     }
 
-    // convert uniqueValues to Vocabulary, excluding infrequent
-    //
+    List<VocabularyWord> getVocabulary(List<ClassifiableText> classifiableTexts) {
+        if (classifiableTexts == null ||
+            classifiableTexts.size() == 0) {
+            throw new IllegalArgumentException();
+        }
 
-    List<VocabularyWord> vocabulary = new ArrayList<>();
+        Map<String, Integer> uniqueValues = new HashMap<>();
 
-    for (Map.Entry<String, Integer> entry : uniqueValues.entrySet()) {
-    	// TODO almakarov
-//      if (entry.getValue() > 3) {
-        vocabulary.add(new VocabularyWord(entry.getKey()));
-//      }
+        // count frequency of use each word (converted to n-gram) from all Classifiable Texts
+
+        for (ClassifiableText classifiableText : classifiableTexts) {
+            for (String word : nGramStrategy.getNGram(classifiableText.getText())) {
+                if (uniqueValues.containsKey(word)) {
+                    // increase counter
+                    uniqueValues.put(word, uniqueValues.get(word) + 1);
+                } else {
+                    // add new word
+                    uniqueValues.put(word, 1);
+                }
+            }
+        }
+
+        // convert uniqueValues to Vocabulary, excluding infrequent
+
+        // TODO: baron - Раньше тут была проверка, что слово встречается не меньше трёх раз
+        final var vocabulary = uniqueValues.keySet().stream().map(VocabularyWord::new).collect(Collectors.toList());
+
+        log.info("Значение - кол-во повторений");
+        for (Entry<String, Integer> f : uniqueValues.entrySet()) {
+            log.info(f.getKey() + " - " + f.getValue());
+        }
+
+        // todo: throw exception if vocabulary is empty
+        return vocabulary;
     }
-    
-    System.out.println("Значение - кол-во повторений");
-    for (Entry<String, Integer> f : uniqueValues.entrySet()) {
-		System.out.println(f.getKey() + " - " + f.getValue());
-	}
-
-    // todo: throw exception if vocabulary is empty
-    return vocabulary;
-  }
 }
--- src/main/java/com/irvil/textclassifier/dao/jdbc/JDBCCharacteristicDAO.java
+++ src/main/java/com/irvil/textclassifier/dao/jdbc/JDBCCharacteristicDAO.java
@@ -11,152 +11,153 @@ import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class JDBCCharacteristicDAO implements CharacteristicDAO {
-  private JDBCConnector connector;
 
-  public JDBCCharacteristicDAO(JDBCConnector connector) {
-    if (connector == null) {
-      throw new IllegalArgumentException();
-    }
+    private JDBCConnector connector;
 
-    this.connector = connector;
-  }
-
-  @Override
-  public List<Characteristic> getAllCharacteristics() {
-    List<Characteristic> characteristics = new ArrayList<>();
-
-    try (Connection con = connector.getConnection()) {
-      String sqlSelect = "SELECT Id, Name FROM CharacteristicsNames";
-      PreparedStatement statement = con.prepareStatement(sqlSelect);
-      ResultSet rs = statement.executeQuery();
-
-      while (rs.next()) {
-        Characteristic characteristic = new Characteristic(rs.getInt("Id"), rs.getString("Name"));
-
-        // get all possible values
-        characteristic.setPossibleValues(getAllPossibleValues(con, characteristic));
-
-        characteristics.add(characteristic);
-      }
-    } catch (SQLException ignored) {
-    	System.out.println(ignored); 
-    }
-
-    return characteristics;
-  }
-
-  @Override
-  public Characteristic addCharacteristic(Characteristic characteristic) throws AlreadyExistsException {
-    if (characteristic == null ||
-        characteristic.getName().equals("") ||
-        characteristic.getPossibleValues() == null ||
-        characteristic.getPossibleValues().size() == 0) {
-      return null;
-    }
-
-    try (Connection con = connector.getConnection()) {
-      if (isCharacteristicExistsInDB(con, characteristic)) {
-        throw new AlreadyExistsException("Characteristic already exists");
-      }
-
-      String sqlInsert = "INSERT INTO CharacteristicsNames (Name) VALUES (?)";
-      PreparedStatement statement = con.prepareStatement(sqlInsert, Statement.RETURN_GENERATED_KEYS);
-      statement.setString(1, characteristic.getName());
-      statement.executeUpdate();
-
-      // get inserted row Id
-      ResultSet generatedKeys = statement.getGeneratedKeys();
-
-      if (generatedKeys.next()) {
-        // set inserted row Id to Characteristic
-        characteristic.setId(generatedKeys.getInt(1));
-
-        // insert possible values
-        //
-
-        characteristic.getPossibleValues().remove(null);
-        characteristic.getPossibleValues().remove(new CharacteristicValue(""));
-
-        for (CharacteristicValue possibleValue : characteristic.getPossibleValues()) {
-          insertPossibleValue(con, characteristic, possibleValue);
+    public JDBCCharacteristicDAO(JDBCConnector connector) {
+        if (connector == null) {
+            throw new IllegalArgumentException();
         }
-      }
-    } catch (SQLException ignored) {
+
+        this.connector = connector;
     }
 
-    return characteristic;
-  }
+    @Override
+    public List<Characteristic> getAllCharacteristics() {
+        List<Characteristic> characteristics = new ArrayList<>();
 
-  private Set<CharacteristicValue> getAllPossibleValues(Connection con, Characteristic characteristic) throws SQLException {
-    Set<CharacteristicValue> possibleValues = new LinkedHashSet<>();
+        try (Connection con = connector.getConnection()) {
+            String sqlSelect = "SELECT Id, Name FROM CharacteristicsNames";
+            PreparedStatement statement = con.prepareStatement(sqlSelect);
+            ResultSet rs = statement.executeQuery();
 
-    String sqlSelect = "SELECT OrderNumber, Value FROM CharacteristicsValues WHERE CharacteristicsNameId = ?";
-    PreparedStatement statement = con.prepareStatement(sqlSelect);
-    statement.setInt(1, characteristic.getId());
-    ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                Characteristic characteristic = new Characteristic(rs.getInt("Id"), rs.getString("Name"));
 
-    while (rs.next()) {
-      possibleValues.add(new CharacteristicValue(rs.getInt("OrderNumber"), rs.getString("Value")));
+                // get all possible values
+                characteristic.setPossibleValues(getAllPossibleValues(con, characteristic));
+
+                characteristics.add(characteristic);
+            }
+        } catch (SQLException ex) {
+            log.warn("SQL Exception", ex);
+        }
+
+        return characteristics;
     }
 
-    return possibleValues;
-  }
+    @Override
+    public Characteristic addCharacteristic(Characteristic characteristic) throws AlreadyExistsException {
+        if (characteristic == null ||
+            characteristic.getName().equals("") ||
+            characteristic.getPossibleValues() == null ||
+            characteristic.getPossibleValues().size() == 0) {
+            return null;
+        }
 
-  private boolean isCharacteristicExistsInDB(Connection con, Characteristic characteristic) throws SQLException {
-    String sqlSelect = "SELECT Id FROM CharacteristicsNames WHERE Name = ?";
-    PreparedStatement statement = con.prepareStatement(sqlSelect);
-    statement.setString(1, characteristic.getName());
-    ResultSet rs = statement.executeQuery();
+        try (Connection con = connector.getConnection()) {
+            if (isCharacteristicExistsInDB(con, characteristic)) {
+                throw new AlreadyExistsException("Characteristic already exists");
+            }
 
-    return rs.next();
-  }
+            String sqlInsert = "INSERT INTO CharacteristicsNames (Name) VALUES (?)";
+            PreparedStatement statement = con.prepareStatement(sqlInsert, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, characteristic.getName());
+            statement.executeUpdate();
 
-  private void insertPossibleValue(Connection con, Characteristic characteristic, CharacteristicValue characteristicValue) throws SQLException {
-    // try to find Value in DB
-    int newCharacteristicValueOrderNumber = searchCharacteristicPossibleValue(con, characteristic, characteristicValue);
+            // get inserted row Id
+            ResultSet generatedKeys = statement.getGeneratedKeys();
 
-    if (newCharacteristicValueOrderNumber == -1) { // not found -> insert it
-      newCharacteristicValueOrderNumber = getLastCharacteristicPossibleValueOrderNumber(con, characteristic) + 1;
+            if (generatedKeys.next()) {
+                // set inserted row Id to Characteristic
+                characteristic.setId(generatedKeys.getInt(1));
 
-      String sqlInsert = "INSERT INTO CharacteristicsValues (OrderNumber, CharacteristicsNameId, Value) VALUES (?, ?, ?)";
-      PreparedStatement statement = con.prepareStatement(sqlInsert);
-      statement.setInt(1, newCharacteristicValueOrderNumber);
-      statement.setInt(2, characteristic.getId());
-      statement.setString(3, characteristicValue.getValue());
-      statement.executeUpdate();
+                // insert possible values
+
+                characteristic.getPossibleValues().remove(null);
+                characteristic.getPossibleValues().remove(new CharacteristicValue(""));
+
+                for (CharacteristicValue possibleValue : characteristic.getPossibleValues()) {
+                    insertPossibleValue(con, characteristic, possibleValue);
+                }
+            }
+        } catch (SQLException ignored) {
+        }
+
+        return characteristic;
     }
 
-    // set inserted row Id to CharacteristicValue
-    characteristicValue.setOrderNumber(newCharacteristicValueOrderNumber);
-  }
+    private Set<CharacteristicValue> getAllPossibleValues(Connection con, Characteristic characteristic) throws SQLException {
+        Set<CharacteristicValue> possibleValues = new LinkedHashSet<>();
 
-  private int searchCharacteristicPossibleValue(Connection con, Characteristic characteristic, CharacteristicValue characteristicValue) throws SQLException {
-    String sqlSelect = "SELECT OrderNumber FROM CharacteristicsValues WHERE CharacteristicsNameId = ? AND Value = ?";
+        String sqlSelect = "SELECT OrderNumber, Value FROM CharacteristicsValues WHERE CharacteristicsNameId = ?";
+        PreparedStatement statement = con.prepareStatement(sqlSelect);
+        statement.setInt(1, characteristic.getId());
+        ResultSet rs = statement.executeQuery();
 
-    PreparedStatement statement = con.prepareStatement(sqlSelect);
-    statement.setInt(1, characteristic.getId());
-    statement.setString(2, characteristicValue.getValue());
-    ResultSet rs = statement.executeQuery();
+        while (rs.next()) {
+            possibleValues.add(new CharacteristicValue(rs.getInt("OrderNumber"), rs.getString("Value")));
+        }
 
-    if (rs.next()) {
-      return rs.getInt("OrderNumber");
+        return possibleValues;
     }
 
-    return -1; // not found in Db
-  }
-
-  private int getLastCharacteristicPossibleValueOrderNumber(Connection con, Characteristic characteristic) throws SQLException {
-    String sqlSelect = "SELECT MAX(OrderNumber) AS MaxOrderNumber FROM CharacteristicsValues WHERE CharacteristicsNameId = ?";
-    PreparedStatement statement = con.prepareStatement(sqlSelect);
-    statement.setInt(1, characteristic.getId());
-    ResultSet rs = statement.executeQuery();
-
-    if (rs.next()) {
-      return rs.getInt("MaxOrderNumber"); // last possible values id
+    private boolean isCharacteristicExistsInDB(Connection con, Characteristic characteristic) throws SQLException {
+        String sqlSelect = "SELECT Id FROM CharacteristicsNames WHERE Name = ?";
+        PreparedStatement statement = con.prepareStatement(sqlSelect);
+        statement.setString(1, characteristic.getName());
+        ResultSet rs = statement.executeQuery();
+        return rs.next();
     }
 
-    return 0; // possible values not found in DB
-  }
+    private void insertPossibleValue(Connection con, Characteristic characteristic, CharacteristicValue characteristicValue) throws SQLException {
+        // try to find Value in DB
+        int newCharacteristicValueOrderNumber = searchCharacteristicPossibleValue(con, characteristic, characteristicValue);
+
+        if (newCharacteristicValueOrderNumber == -1) { // not found -> insert it
+            newCharacteristicValueOrderNumber = getLastCharacteristicPossibleValueOrderNumber(con, characteristic) + 1;
+
+            String sqlInsert = "INSERT INTO CharacteristicsValues (OrderNumber, CharacteristicsNameId, Value) VALUES (?, ?, ?)";
+            PreparedStatement statement = con.prepareStatement(sqlInsert);
+            statement.setInt(1, newCharacteristicValueOrderNumber);
+            statement.setInt(2, characteristic.getId());
+            statement.setString(3, characteristicValue.getValue());
+            statement.executeUpdate();
+        }
+
+        // set inserted row Id to CharacteristicValue
+        characteristicValue.setOrderNumber(newCharacteristicValueOrderNumber);
+    }
+
+    private int searchCharacteristicPossibleValue(Connection con, Characteristic characteristic, CharacteristicValue characteristicValue) throws SQLException {
+        String sqlSelect = "SELECT OrderNumber FROM CharacteristicsValues WHERE CharacteristicsNameId = ? AND Value = ?";
+
+        PreparedStatement statement = con.prepareStatement(sqlSelect);
+        statement.setInt(1, characteristic.getId());
+        statement.setString(2, characteristicValue.getValue());
+        ResultSet rs = statement.executeQuery();
+
+        if (rs.next()) {
+            return rs.getInt("OrderNumber");
+        }
+
+        return -1; // not found in Db
+    }
+
+    private int getLastCharacteristicPossibleValueOrderNumber(Connection con, Characteristic characteristic) throws SQLException {
+        String sqlSelect = "SELECT MAX(OrderNumber) AS MaxOrderNumber FROM CharacteristicsValues WHERE CharacteristicsNameId = ?";
+        PreparedStatement statement = con.prepareStatement(sqlSelect);
+        statement.setInt(1, characteristic.getId());
+        ResultSet rs = statement.executeQuery();
+
+        if (rs.next()) {
+            return rs.getInt("MaxOrderNumber"); // last possible values id
+        }
+
+        return 0; // possible values not found in DB
+    }
 }
